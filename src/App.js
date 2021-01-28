4c60728bb5e35070fa266ce81ac02e2cfc12d6c9
JenniferJohnstone <jennifermaryjohnstone@gmail.com>
2021-01-28 13:38:05 +1100

    timer fully working + styling

--- src/App.js
+++ src/App.js
@@ -2,24 +2,25 @@ import './App.css';
 import Clock from './clock'
 import { useState } from 'react';
 import React from 'react'
-// import Timer from './timer'
 
 
 function App() {
 
-  // finding today's date and time
   const [startCounter, setStartCounter] = useState(false);
-
-
-
-
+  const [timeType,  setTimeType] = useState(1500)
+  
   return (
 
     <div className="App">
 
-      <Clock start = {startCounter} />
+      <Clock start = {startCounter} timeType = {timeType} />
       <button onClick = {() => setStartCounter(true)}>Start</button>
       <button onClick = {() => setStartCounter(false)}>Stop</button>
+      <br></br>
+      <button onClick = {() => setTimeType(1500)}>Pomodoro</button>
+      <button onClick = {() => setTimeType(300)}>Short Break</button>
+      <button onClick = {() => setTimeType(900)}>Long Break</button>
+
     </div>
   );
 }
--- src/clock.js
+++ src/clock.js
@@ -1,39 +1,50 @@
 import {useState} from 'react'
+import './App.css';
 
-var Clock = ({start}) => {
 
-    const [ counter, setCounter ] = useState(100)
-    var today = new Date()
-    var time = today.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
+var Clock = ({start, timeType}) => {
 
-    const style = {
-        fontSize: '50px',
-        color: 'white', 
-        backgroundColor: 'black',
-        marginLeft: '80%',
-    }
+    const [counter, setCounter ] = useState(0)
+    var secondsPassed = timeType + counter
+    const [today, setToday] = useState(new Date())
 
     if (start === true){
         setTimeout(() => {
+            console.log('this is counter',  counter)
             setCounter(counter - 1)
         }, 1000);
     }
 
     if(start === false){
-        setInterval(() => {
-            today = new Date()
-            time = today.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
+        setTimeout(() => {
+            setToday(new Date())
             console.log('updating the time')
-            //this makes it so the time will keep updating even if the timer isn't running
+            //this makes it so the time will keep updating (every 30 seconds) even if the timer isn't running
         }, 60000);
     }
 
-    
+    var time = today.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
+    //formatting the time to display
+
+    var minutes = Math.floor(secondsPassed / 60);
+    var seconds = secondsPassed - minutes * 60;
+    //a little math to calculate minutes and seconds from the timer (which is in seconds)
+
+    function str_pad_left(string,pad,length) {
+        return (new Array(length+1).join(pad)+string).slice(-length);
+    }
+    var finalTime = str_pad_left(minutes,'0',2)+':'+str_pad_left(seconds,'0',2);
+    //formatting so the time will display as 00:00 
+
+
+
+
     return (
 
         <>
-        <p style = {style}>{time}</p>
-        <p>{counter}</p>
+        <p className = 'ClockStyle'>{time}</p>
+        <p className = 'timerStyle'>{finalTime}</p>
+        <button onClick = {() => setCounter(0)}>Reset</button>
         </>
     )
 }
